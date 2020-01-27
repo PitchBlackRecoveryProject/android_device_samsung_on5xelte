13987f2a628cac03ba061262022ecf500261ea12
rokibhasansagar <rokibhasansagar2014@outlook.com>
2020-01-27 21:11:13 +0600

    Bringup PBRP for samsung on5xelte

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,25 @@
-#Bootloader 
+##
+# Copyright (C) 2020 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##
 
+# Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := universal7570
 
 # Platform
-
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOARD_PLATFORM_GPU := mali-t720
 
@@ -14,8 +29,8 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 :=armeabi
 TARGET_CPU_VARIANT := cortex-a53
-#kernel
 
+# Kernel
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_USES_UNCOMPRESSED_KERNEL := true
@@ -23,8 +38,7 @@ TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_PREBUILT_KERNEL := device/samsung/on5xelte/Image
 TARGET_PREBUILT_DTB := device/samsung/on5xelte/dt.img
 
-#BOOT image 
-
+# BOOT image
 BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -40,13 +54,11 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12075401216  
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-
 # File System
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -55,13 +67,14 @@ TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 #BOARD_HAS_NO_REAL_SDCARD := true
 TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 162
+TW_DEFAULT_BRIGHTNESS := 115
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_NTFS_3G := true
-TW_EXTRA_LANGUAGES := true
-
+TW_INCLUDE_FUSE_EXFAT := true
+TW_DEFAULT_LANGUAGE := en
+TW_EXTRA_LANGUAGES := false
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
@@ -72,4 +85,4 @@ TW_INCLUDE_CRYPTO := true
 #TWRP_EVENT_LOGGING := true
 TW_NEVER_UNMOUNT_SYSTEM := true
 #TARGET_USES_LOGD := true
-#TWRP_INCLUDE_LOGCAT := true
+TWRP_INCLUDE_LOGCAT := true
--- omni_on5xelte.mk
+++ omni_on5xelte.mk
@@ -1,15 +1,66 @@
+##
+# Copyright (C) 2020 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##
+
 # Release name
 PRODUCT_RELEASE_NAME := on5xelte
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
+
+PRODUCT_LOCALES := en_US
+
+# From runtime_libart.mk
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    dalvik.vm.image-dex2oat-Xms=64m \
+    dalvik.vm.image-dex2oat-Xmx=64m \
+    dalvik.vm.dex2oat-Xms=64m \
+    dalvik.vm.dex2oat-Xmx=512m \
+    ro.dalvik.vm.native.bridge=0
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    pm.dexopt.first-boot=interpret-only \
+    pm.dexopt.boot=verify-profile \
+    pm.dexopt.install=interpret-only \
+    pm.dexopt.bg-dexopt=speed-profile \
+    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.nsys-library=speed \
+    pm.dexopt.shared-apk=speed \
+    pm.dexopt.forced-dexopt=speed \
+    pm.dexopt.core-app=speed \
+    dalvik.vm.image-dex2oat-filter=verify-at-runtime \
+    dalvik.vm.dex2oat-filter=verify-at-runtime \
+    dalvik.vm.usejit=true
+
+# adb is there, so just add mtp for now
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# From core_tiny.mk
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.zygote=zygote32
+PRODUCT_COPY_FILES += \
+    system/core/rootdir/init.zygote32.rc:root/init.zygote32.rc
 
 PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
+    charger_res_images \
+    charger
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_on5xelte
